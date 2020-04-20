0397ca240f81cac08c76172df8625151f9fae8f8
Shivam Gera <shivam.g@tapchief.com>
2020-04-20 20:54:01 +0530

    more fixes

--- redisclone/utils.py
+++ redisclone/utils.py
@@ -4,7 +4,7 @@ from sortedcontainers import SortedList, SortedSet, SortedDict
 from apscheduler.schedulers.background import BackgroundScheduler
 scheduler = BackgroundScheduler()
 import json
-
+import pickle
 
 key_value_pair = {}
 zdata_store = {}
@@ -72,27 +72,28 @@ def get_values_for_range(key, start, stop):
 
 
 def save_data_in_disk():
-    with open('key_value_pair.json', 'w') as fp:
-        json.dump(key_value_pair, fp)
-    with open('zdata_store.json', 'w') as fp:
-        json.dump(zdata_store, fp, default=lambda o: o.__dict__)
-    with open('timeout_key.json', 'w') as fp:
-        json.dump(timeout_key, fp, default=str)
+    with open('key_value_pair.pkl', 'wb') as fp:
+        pickle.dump(key_value_pair, fp)
+    with open('zdata_store.pkl', 'wb') as fp:
+        pickle.dump(zdata_store, fp)
+    with open('timeout_key.pkl', 'wb') as fp:
+        pickle.dump(timeout_key, fp)
 
 def load_data_on_start():
     global key_value_pair, zdata_store, timeout_key
-    with open('key_value_pair.json', 'r') as fp:
-        key_value_pair = json.load(fp)
-    with open('zdata_store.json', 'r') as fp:
-        zdata_store = json.load(fp)
-    with open('timeout_key.json', 'r') as fp:
-        timeout_key = json.load(fp)
+    with open('key_value_pair.pkl', 'rb') as fp:
+        key_value_pair = pickle.load(fp)
+    with open('zdata_store.pkl', 'rb') as fp:
+        zdata_store = pickle.load(fp)
+    with open('timeout_key.pkl', 'rb') as fp:
+        timeout_key = pickle.load(fp)
     # scheduler.remove_job('1')
 
 
 def add_jobs():
-    # load_data_on_start()
+    scheduler.start()
     # job = scheduler.add_job(load_data_on_start, 'interval', seconds=1, id='1',replace_existing=False)
+    load_data_on_start()
     job = scheduler.add_job(save_data_in_disk,'interval' , seconds=10, replace_existing=False)
     job = scheduler.add_job(delete_key_value, 'interval' , seconds=6, replace_existing=True)
-    scheduler.start()
+    
